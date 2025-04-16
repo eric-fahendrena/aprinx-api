50c4bf861a72c5760271ed023dd1367f6c0d8456
Eric Fahendrena <ericfahendrena29@gmail.com>
2025-04-16 11:10:15 +0300

    added some infos to transaction results

--- models/courseTransaction.model.js
+++ models/courseTransaction.model.js
@@ -27,7 +27,7 @@ export const insertTransaction = async (tData) => {
  * @returns {Array<object>}
  */
 export const selectTransactionsByCourseAuthor = async (cAuthorId, offset, limit) => {
-  const query = "SELECT course_transactions.*, courses.author_id AS course_author_id, users.name AS buyer_name FROM course_transactions INNER JOIN courses ON courses.id = course_transactions.course_id INNER JOIN users ON users.id = course_transactions.buyer_id WHERE courses.author_id = $1 AND course_transactions.status = 'PENDING' OFFSET $2 LIMIT $3";
+  const query = "SELECT course_transactions.*, courses.price AS amount, courses.title AS course_title, courses.author_id AS course_author_id, users.name AS buyer_name FROM course_transactions INNER JOIN courses ON courses.id = course_transactions.course_id INNER JOIN users ON users.id = course_transactions.buyer_id WHERE courses.author_id = $1 AND course_transactions.status = 'PENDING' OFFSET $2 LIMIT $3";
   const result = await pool.query(query, [ cAuthorId, offset, limit ]);
   return result.rows;
 }
@@ -57,11 +57,30 @@ export const updateTransactionStatus = async (transId, status) => {
   return result.rows[0];
 }
 
+/**
+ * Selects course transaction
+ * 
+ * @param {object} tData 
+ * @returns 
+ */
 export const selectTransaction = async (tData) => {
   const query = "SELECT course_transactions.*, users.name AS buyer_name, users.picture AS buyer_picture FROM course_transactions INNER JOIN users ON users.id = course_transactions.buyer_id INNER JOIN courses ON courses.id = course_transactions.course_id WHERE courses.author_id = $1 AND trans_id = $2";
   const result = await pool.query(query, [
     tData.course_author_id,
     tData.trans_id,
   ]);
+  return result.rows[0];
+}
+
+/**
+ * Selects latest transaction by author id
+ * 
+ * @param {number} authorId 
+ * @param {number} buyerId 
+ * @returns 
+ */
+export const selectLatestTransByAuthorId = async (authorId, buyerId) => {
+  const query = "SELECT * FROM course_transactions WHERE author_id = $1 AND buyer_id = $2 ORDER BY id DESC";
+  const result = await pool.query(query, [ authorId, buyerId ]);
   return result.rows[0];
 }
